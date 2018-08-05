88a69035ee4b27e1bc3fde25012cf5af133c9620
AntalEhrbar <ehranti@gmail.com>
2018-08-05 21:56:36 +0200

    Failed to connect couchbase

---
+++ probafeladat/ClusterConnect.cs
@@ -0,0 +1,35 @@
+﻿using Couchbase;
+using Couchbase.Authentication;
+using Couchbase.Configuration.Client;
+using System;
+using System.Collections.Generic;
+
+namespace probafeladat
+{
+    public class ClusterCOnnect
+    {
+        public ClusterCOnnect()
+        {
+
+            var config = new ClientConfiguration
+            {
+                Servers = new List<Uri> {
+        new Uri("http://192.168.56.101:8091"),
+        new Uri("http://192.168.56.102:8091")
+    },
+                BucketConfigs = new Dictionary<string, BucketConfiguration> {{
+            "mybucket", new BucketConfiguration{
+                BucketName = "mybucket"
+            }
+        }
+     }
+            };
+            //create the authenticator for passing in the user and password for RBAC
+            var credentials = new PasswordAuthenticator("Administrator", "password");
+ 
+            ClusterHelper.Initialize(config, credentials);
+            var bucket = ClusterHelper.GetBucket("mybucket");
+
+        }
+    }
+}
--- probafeladat/Program.cs
+++ probafeladat/Program.cs
@@ -7,6 +7,8 @@ namespace probafeladat
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            new ClusterCOnnect();
+            Console.ReadLine();
         }
     }
 }
